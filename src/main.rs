9f698ddd56424c43adf057680042a800a17e4e1a
Rudolph Almeida <rudolf1.almeida@gmail.com>
2024-08-27 20:02:21 +1000

    Update all deps

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ fn main() -> Result<(), eframe::Error> {
     env_logger::init();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([1240.0, 760.0]),
-        // TODO: Separate framerate from monitor
+        // TODO: Separate frame rate from monitor
         vsync: true,
         ..Default::default()
     };
     eframe::run_native(
         "GiBi",
         options,
-        Box::new(|cc| Box::<GameboyApp>::new(GameboyApp::new(cc))),
+        Box::new(|cc| Ok(Box::<GameboyApp>::new(GameboyApp::new(cc)))),
     )
 }
