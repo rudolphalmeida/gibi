c06da6ade5d60ecfa82fde64c96f71c323ff05d3
Rudolph Almeida <rudolf1.almeida@gmail.com>
2026-01-24 17:01:33 +1100

    Download file directly to roms/

--- install-deps.sh
+++ install-deps.sh
@@ -3,9 +3,7 @@
 set -x
 
 mkdir roms
-cd roms
 echo "Fetching CGB bootrom"
-wget https://gbdev.gg8.se/files/roms/bootroms/cgb_boot.bin
-cd ..
+wget -O roms/cgb_boot.bin https://gbdev.gg8.se/files/roms/bootroms/cgb_boot.bin
 
 sudo apt install libasound2-dev
